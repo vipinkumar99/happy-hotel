11d52f6a4c5875943c4296950ee0d750cb52ef2b
vipinkumar999945@gmail.com <vipinkumar999945@gmail.com>
2021-06-29 07:29:10 +0530

    second commit

--- src/test/java/com/happy/hotel/HappyHotelApplicationTests.java
+++ src/test/java/com/happy/hotel/HappyHotelApplicationTests.java
@@ -3,11 +3,15 @@ package com.happy.hotel;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootTest
 class HappyHotelApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.info("Test is started!");
 	}
 
 }
