78b9841111797f5e102f988420325952bdd7842a
vipinkumar999945@gmail.com <vipinkumar999945@gmail.com>
2021-06-29 08:47:42 +0530

    remove logger

--- src/test/java/com/happy/hotel/HappyHotelApplicationTests.java
+++ src/test/java/com/happy/hotel/HappyHotelApplicationTests.java
@@ -5,15 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @SpringBootTest
 class HappyHotelApplicationTests {
 
 	@Test
 	void contextLoads() {
-		log.info("Test is started!");
 		assertEquals(true, true);
 	}
 
